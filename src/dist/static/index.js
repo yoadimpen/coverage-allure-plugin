895ae9bd5c8958e6655e27f81610774221817e66
Yoana Penkova <yoadimpen@alum.us.es>
2021-03-17 11:08:20 +0100

    ST017/002: widget feedback applied

--- src/dist/static/index.js
+++ src/dist/static/index.js
@@ -64,7 +64,169 @@ class MyWidget extends Backbone.Marionette.View {
 
 const template2 = function (data) {
 
-    var html = "";
+    var html = "";'use strict';
+
+    var MyTabModel = Backbone.Collection.extend({
+        url: 'data/coverage.json'
+    })
+    
+    class MyLayout extends allure.components.AppLayout {
+    
+        initialize() {
+            this.model = new MyTabModel();
+        }
+    
+        loadData() {
+            return this.model.fetch();
+        }
+    
+        getContentView() {
+            return new MyView({items: this.model.attributes});
+        }
+    }
+    
+    const template = function (data) {
+    
+        var html = '<h3 class="pane__title">Coverage</h3>';
+        return html;
+    }
+    
+    var MyView = Backbone.Marionette.View.extend({
+        template: template,
+    
+        render: function () {
+            this.$el.html(this.template(this.options));
+            return this;
+        }
+    })
+    
+    allure.api.addTab('coverage', {
+        title: 'Coverage', icon: 'fa fa-pie-chart',
+        route: 'coverage',
+        onEnter: (function () {
+            return new MyLayout()
+        })
+    });
+    
+    class MyWidget extends Backbone.Marionette.View {
+    
+        template(data) {
+            return widgetTemplate(data)
+        }
+    
+        serializeData() {
+            return {
+                items: this.model.get('items'),
+            }
+        }
+    }
+    
+    const template2 = function (data) {
+    
+        var html = "";
+    
+        var initDiv = document.createElement("div");
+        initDiv.setAttribute("class", "container");
+    
+        var div = document.createElement("div");
+        div.setAttribute("class", "row");
+        div.setAttribute("style", "display: flex");
+    
+        var textDiv = document.createElement("div");
+        textDiv.setAttribute("class", "col");
+        textDiv.setAttribute("style", "width: 50%;");
+    
+        var titleCoverage = document.createElement("h2");
+        titleCoverage.setAttribute("class", "widget__title");
+        titleCoverage.appendChild(document.createTextNode("coverage"));
+    
+        var levelCoverage = document.createElement("p");
+        levelCoverage.appendChild(document.createTextNode("Test coverage level: " + this.model.attributes.coverageLevel));
+    
+        var totalCoverage = document.createElement("p");
+        totalCoverage.appendChild(document.createTextNode("Total coverage (outer ring): " + this.model.attributes.totalCoverage.toFixed(2) + "%"));
+    
+        var inputCoverage = document.createElement("p");
+        inputCoverage.appendChild(document.createTextNode("Input coverage (middle ring): " + this.model.attributes.inputCoverage.toFixed(2) + "%"));
+    
+        var outputCoverage = document.createElement("p");
+        outputCoverage.appendChild(document.createTextNode("Output coverage (inner ring): " + this.model.attributes.outputCoverage.toFixed(2) + "%"));
+    
+        var chartDiv = document.createElement("div");
+        chartDiv.setAttribute("class", "col");
+        chartDiv.setAttribute("style", "width: 50%;");
+    
+        var canvas = document.createElement("canvas");
+        canvas.setAttribute("id", "myChart");
+        canvas.setAttribute("style", "margin-bottom: 4%; max-width: 545px; max-height: 285px;");
+    
+        chartDiv.appendChild(canvas);
+        var ctx = canvas.getContext('2d');
+    
+        var chart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: [
+                'Covered',
+                'Not covered'
+                ],
+                datasets: [
+                    {
+                        data: [this.model.attributes.totalCoverage.toFixed(2), (100-this.model.attributes.totalCoverage).toFixed(2)],
+                        backgroundColor: [
+                            'rgb(151, 204, 100)',
+                            'rgb(253, 90, 62)'
+                        ]
+                    },
+                    {
+                        data: [this.model.attributes.inputCoverage.toFixed(2), (100-this.model.attributes.inputCoverage).toFixed(2)],
+                        backgroundColor: [
+                            'rgb(151, 204, 100)',
+                            'rgb(253, 90, 62)'
+                        ]
+                    }, 
+                    {
+                        data: [this.model.attributes.outputCoverage.toFixed(2), (100-this.model.attributes.outputCoverage).toFixed(2)],
+                        backgroundColor: [
+                            'rgb(151, 204, 100)',
+                            'rgb(253, 90, 62)'
+                        ]
+                    }
+                ]
+            },
+            options: {
+                legend: false
+            }
+        });
+    
+        textDiv.appendChild(titleCoverage);
+        textDiv.appendChild(levelCoverage);
+        textDiv.appendChild(totalCoverage);
+        textDiv.appendChild(inputCoverage);
+        textDiv.appendChild(outputCoverage);
+    
+        div.appendChild(textDiv);
+        div.appendChild(chartDiv);
+    
+        initDiv.appendChild(div);
+    
+        var divs = document.querySelectorAll("[data-id='coverage']");
+        var divToAdd = divs[0].children[1];
+        divToAdd.appendChild(initDiv);
+    
+        return html;
+    }
+    
+    var MyView2 = Backbone.Marionette.View.extend({
+        template: template2,
+    
+        render: function () {
+            this.$el.html(this.template(this.options));
+            return this;
+        }
+    })
+    
+    allure.api.addWidget('widgets', 'coverage', MyView2);
 
     var initDiv = document.createElement("div");
     initDiv.setAttribute("class", "container");
--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -8,10 +8,6 @@ class MyLayout extends allure.components.AppLayout {
 
     initialize() {
         this.model = new MyTabModel();
-
-        /*this.model = {
-          "test" : "this is a test"
-        }*/
     }
 
     loadData() {
@@ -19,12 +15,13 @@ class MyLayout extends allure.components.AppLayout {
     }
 
     getContentView() {
-        return new MyView({items: this.model});
+        return new MyView({items: this.model.attributes});
     }
 }
 
 const template = function (data) {
-    html = '<h3 class="pane__title">My Tab</h3>';
+
+    var html = '<h3 class="pane__title">Coverage</h3>';
     return html;
 }
 
@@ -37,9 +34,9 @@ var MyView = Backbone.Marionette.View.extend({
     }
 })
 
-allure.api.addTab('mytab', {
-    title: 'About', icon: 'fa fa-info-circle',
-    route: 'mytab',
+allure.api.addTab('coverage', {
+    title: 'Coverage', icon: 'fa fa-pie-chart',
+    route: 'coverage',
     onEnter: (function () {
         return new MyLayout()
     })
@@ -48,7 +45,7 @@ allure.api.addTab('mytab', {
 class MyWidget extends Backbone.Marionette.View {
 
     template(data) {
-            return widgetTemplate(data)
+        return widgetTemplate(data)
     }
 
     serializeData() {
@@ -58,4 +55,288 @@ class MyWidget extends Backbone.Marionette.View {
     }
 }
 
-allure.api.addWidget('coverage', MyWidget);
+/*allure.api.addWidget('widgets', 'coverage', allure.components.WidgetStatusView.extend({
+    rowTag: 'a',
+    title: 'coverage',
+    baseUrl: 'coverage',
+    showAll: true
+}));*/
+
+const template2 = function (data) {
+
+    var html = "";'use strict';
+
+    var MyTabModel = Backbone.Collection.extend({
+        url: 'data/coverage.json'
+    })
+    
+    class MyLayout extends allure.components.AppLayout {
+    
+        initialize() {
+            this.model = new MyTabModel();
+        }
+    
+        loadData() {
+            return this.model.fetch();
+        }
+    
+        getContentView() {
+            return new MyView({items: this.model.attributes});
+        }
+    }
+    
+    const template = function (data) {
+    
+        var html = '<h3 class="pane__title">Coverage</h3>';
+        return html;
+    }
+    
+    var MyView = Backbone.Marionette.View.extend({
+        template: template,
+    
+        render: function () {
+            this.$el.html(this.template(this.options));
+            return this;
+        }
+    })
+    
+    allure.api.addTab('coverage', {
+        title: 'Coverage', icon: 'fa fa-pie-chart',
+        route: 'coverage',
+        onEnter: (function () {
+            return new MyLayout()
+        })
+    });
+    
+    class MyWidget extends Backbone.Marionette.View {
+    
+        template(data) {
+            return widgetTemplate(data)
+        }
+    
+        serializeData() {
+            return {
+                items: this.model.get('items'),
+            }
+        }
+    }
+    
+    const template2 = function (data) {
+    
+        var html = "";
+    
+        var initDiv = document.createElement("div");
+        initDiv.setAttribute("class", "container");
+    
+        var div = document.createElement("div");
+        div.setAttribute("class", "row");
+        div.setAttribute("style", "display: flex");
+    
+        var textDiv = document.createElement("div");
+        textDiv.setAttribute("class", "col");
+        textDiv.setAttribute("style", "width: 50%;");
+    
+        var titleCoverage = document.createElement("h2");
+        titleCoverage.setAttribute("class", "widget__title");
+        titleCoverage.appendChild(document.createTextNode("coverage"));
+    
+        var levelCoverage = document.createElement("p");
+        levelCoverage.appendChild(document.createTextNode("Test coverage level: " + this.model.attributes.coverageLevel));
+    
+        var totalCoverage = document.createElement("p");
+        totalCoverage.appendChild(document.createTextNode("Total coverage (outer ring): " + this.model.attributes.totalCoverage.toFixed(2) + "%"));
+    
+        var inputCoverage = document.createElement("p");
+        inputCoverage.appendChild(document.createTextNode("Input coverage (middle ring): " + this.model.attributes.inputCoverage.toFixed(2) + "%"));
+    
+        var outputCoverage = document.createElement("p");
+        outputCoverage.appendChild(document.createTextNode("Output coverage (inner ring): " + this.model.attributes.outputCoverage.toFixed(2) + "%"));
+    
+        var chartDiv = document.createElement("div");
+        chartDiv.setAttribute("class", "col");
+        chartDiv.setAttribute("style", "width: 50%;");
+    
+        var canvas = document.createElement("canvas");
+        canvas.setAttribute("id", "myChart");
+        canvas.setAttribute("style", "margin-bottom: 4%; max-width: 545px; max-height: 285px;");
+    
+        chartDiv.appendChild(canvas);
+        var ctx = canvas.getContext('2d');
+    
+        var chart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: [
+                'Covered',
+                'Not covered'
+                ],
+                datasets: [
+                    {
+                        data: [this.model.attributes.totalCoverage.toFixed(2), (100-this.model.attributes.totalCoverage).toFixed(2)],
+                        backgroundColor: [
+                            'rgb(151, 204, 100)',
+                            'rgb(253, 90, 62)'
+                        ]
+                    },
+                    {
+                        data: [this.model.attributes.inputCoverage.toFixed(2), (100-this.model.attributes.inputCoverage).toFixed(2)],
+                        backgroundColor: [
+                            'rgb(151, 204, 100)',
+                            'rgb(253, 90, 62)'
+                        ]
+                    }, 
+                    {
+                        data: [this.model.attributes.outputCoverage.toFixed(2), (100-this.model.attributes.outputCoverage).toFixed(2)],
+                        backgroundColor: [
+                            'rgb(151, 204, 100)',
+                            'rgb(253, 90, 62)'
+                        ]
+                    }
+                ]
+            },
+            options: {
+                legend: false
+            }
+        });
+    
+        textDiv.appendChild(titleCoverage);
+        textDiv.appendChild(levelCoverage);
+        textDiv.appendChild(totalCoverage);
+        textDiv.appendChild(inputCoverage);
+        textDiv.appendChild(outputCoverage);
+    
+        div.appendChild(textDiv);
+        div.appendChild(chartDiv);
+    
+        initDiv.appendChild(div);
+    
+        var divs = document.querySelectorAll("[data-id='coverage']");
+        var divToAdd = divs[0].children[1];
+        divToAdd.appendChild(initDiv);
+    
+        return html;
+    }
+    
+    var MyView2 = Backbone.Marionette.View.extend({
+        template: template2,
+    
+        render: function () {
+            this.$el.html(this.template(this.options));
+            return this;
+        }
+    })
+    
+    allure.api.addWidget('widgets', 'coverage', MyView2);
+
+    var initDiv = document.createElement("div");
+    initDiv.setAttribute("class", "container");
+
+    var div = document.createElement("div");
+    div.setAttribute("class", "row");
+    div.setAttribute("style", "display: flex");
+
+    var textDiv = document.createElement("div");
+    textDiv.setAttribute("class", "col");
+    textDiv.setAttribute("style", "width: 50%;");
+
+    var titleCoverage = document.createElement("h2");
+    titleCoverage.setAttribute("class", "widget__title");
+    titleCoverage.appendChild(document.createTextNode("coverage"));
+
+    var levelText = document.createElement("p");
+    levelText.appendChild(document.createTextNode("Level of coverage: " + this.model.attributes.coverageLevel));
+
+    var legendTitle = document.createElement("p");
+    legendTitle.appendChild(document.createTextNode("Chart's sections:"));
+
+    var legendList = document.createElement("ul");
+
+    var outerLi = document.createElement("li");
+    outerLi.setAttribute("style", "margin-bottom: 1%;");
+    outerLi.appendChild(document.createTextNode("Outer: Total coverage"));
+
+    var middleLi = document.createElement("li");
+    middleLi.setAttribute("style", "margin-bottom: 1%;");
+    middleLi.appendChild(document.createTextNode("Middle: Input coverage"));
+
+    var innerLi = document.createElement("li");
+    innerLi.appendChild(document.createTextNode("Inner: Output coverage"));
+
+    legendList.appendChild(outerLi);
+    legendList.appendChild(middleLi);
+    legendList.appendChild(innerLi);
+
+    var chartDiv = document.createElement("div");
+    chartDiv.setAttribute("class", "col");
+    chartDiv.setAttribute("style", "width: 50%;");
+
+    var canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "myChart");
+    canvas.setAttribute("style", "margin-bottom: 4%; max-width: 545px; max-height: 285px;");
+
+    chartDiv.appendChild(canvas);
+    var ctx = canvas.getContext('2d');
+
+    var chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: [
+            'Covered',
+            'Not covered'
+            ],
+            datasets: [
+                {
+                    data: [this.model.attributes.totalCoverage.toFixed(2), (100-this.model.attributes.totalCoverage).toFixed(2)],
+                    backgroundColor: [
+                        'rgb(151, 204, 100)',
+                        'rgb(253, 90, 62)'
+                    ]
+                },
+                {
+                    data: [this.model.attributes.inputCoverage.toFixed(2), (100-this.model.attributes.inputCoverage).toFixed(2)],
+                    backgroundColor: [
+                        'rgb(151, 204, 100)',
+                        'rgb(253, 90, 62)'
+                    ]
+                }, 
+                {
+                    data: [this.model.attributes.outputCoverage.toFixed(2), (100-this.model.attributes.outputCoverage).toFixed(2)],
+                    backgroundColor: [
+                        'rgb(151, 204, 100)',
+                        'rgb(253, 90, 62)'
+                    ]
+                }
+            ]
+        },
+        options: {
+            legend: false
+        }
+    });
+
+    textDiv.appendChild(titleCoverage);
+    textDiv.appendChild(levelText);
+    textDiv.appendChild(legendTitle);
+    textDiv.appendChild(legendList);
+
+    div.appendChild(textDiv);
+    div.appendChild(chartDiv);
+
+    initDiv.appendChild(div);
+
+    var divs = document.querySelectorAll("[data-id='coverage']");
+    var divToAdd = divs[0].children[1];
+    divToAdd.appendChild(initDiv);
+
+    return html;
+}
+
+var MyView2 = Backbone.Marionette.View.extend({
+    template: template2,
+
+    render: function () {
+        this.$el.html(this.template(this.options));
+        return this;
+    }
+})
+
+allure.api.addWidget('widgets', 'coverage', MyView2);
